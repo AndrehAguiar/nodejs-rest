c5119a40f3eed086eaa151afe5da06cfd66b88c8
AndrehAguiar <andreaguiar.g@gmail.com>
2021-09-10 21:24:14 -0300

    Valida usuário com Basic Auth.. JWT

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,39 +1,18 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { ForbiddenError } from "../models/catchs/forbidden.error.model";
-import userRepository from "../repositories/user.repository";
 import JWT from 'jsonwebtoken';
-
+import basicAuthenticationMiddleware from "../middlewares/basic.authentication.middleware";
+import { ForbiddenError } from "../models/catchs/forbidden.error.model";
 
 const authorizationRoute = Router();
 
-authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
+authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
 
     try {
-        const authorizationHeader = req.headers['authorization'];
+        const user = req.user;
 
-        if (!authorizationHeader) {
-            throw new ForbiddenError("Credenciais não informadas");
+        if (!user) {
+            throw new ForbiddenError("Usuário não informado");
         }
-
-        const [authenticationType, token] = authorizationHeader.split(' ');
-
-        if (authenticationType !== 'Basic' || !token) {
-            throw new ForbiddenError('Tipo de autenticação inválido');
-        }
-
-        const tokenContent = Buffer.from(token, 'base64').toString('utf-8');
-
-        const [username, password] = tokenContent.split(':');
-
-        if (!username || !password) {
-            throw new ForbiddenError('Credenciais não preechidas.');
-        }
-
-        const user = await userRepository.findUsernameAndPassword(username, password);
-
-        if(!user){
-            throw new ForbiddenError('Usuário e senha inválidos!');
-        } 
 
         const jwtPayload = { username: user.username };
         const jwtOptions = { subject: user?.uuid };
